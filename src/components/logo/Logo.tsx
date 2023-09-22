644030b098e9722e58cc1f5e4ef6508c0f845c7f
Vital <Vitalii8881878@gmail.com>
2023-09-22 07:26:21 -0400

    add new folder

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+            <a href="">
+               <Icon iconId={"code"}/>
+            </a>
+    );
+};
+
+export default Logo;
+
