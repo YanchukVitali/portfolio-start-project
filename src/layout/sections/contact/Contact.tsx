7fecc7d392cf0cf6211a6a1af578643da4da8ec9
Vital <Vitalii8881878@gmail.com>
2023-09-26 21:20:02 -0400

    add folder contact

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
+import {Button} from "../../../components/Button";
 
 export const Contact = () => {
     return (
         <StyledContact>
-<SectionTitle>Contact</SectionTitle>
+            <SectionTitle>Contact</SectionTitle>
+            <StyledForm>
+                <Field placeholder={"name"}/>
+                <Field placeholder={"subject"}/>
+                <Field placeholder={"message"} as={"textarea"}/>
+                <Button type={"submit"}>Send message</Button>
+            </StyledForm>
         </StyledContact>
     );
 };
 
 const StyledContact = styled.section`
   background-color: #8ccc8c;
+  min-height: 50vh;
 `
+const StyledForm = styled.form`
+max-width: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 0 auto;
+  
+`
+const Field = styled.input`
+
+`
+
