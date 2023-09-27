be077e0b1e306b9993095778399abb8f72267c81
Vital <Vitalii8881878@gmail.com>
2023-09-26 21:02:27 -0400

    add folder testimony

---
+++ src/layout/sections/contact/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+
+export const Contact = () => {
+    return (
+        <StyledContact>
+<SectionTitle>Contact</SectionTitle>
+        </StyledContact>
+    );
+};
+
+const StyledContact = styled.section`
+  background-color: #8ccc8c;
+`
